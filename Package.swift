b0f032526d31020f36572685245bb1bff6898506
Himanshu Neema <himanshu@upvpn.app>
2024-09-13 13:35:26 -0700

    add tvOS(.v17) platform

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "WireGuardKit",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15)
+        .iOS(.v15),
+        .tvOS(.v17)
     ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
